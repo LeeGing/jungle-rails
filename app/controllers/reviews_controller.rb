24774c306ad9e283f4a1fa24340ebc967d1492ea
Edmond Lee <e.lee@live.ca>
2017-09-02 00:43:22 +0000

    Foundation complete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,38 @@
 class ReviewsController < ApplicationController
+    
+    before_action :require_login
+ 
 	def create
-  	raise "Yay, I'm here!"
+    @product = Product.find(params[:product_id])
+
+    # if no product escape from method, redirect
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+
+	  if @review.save
+	    redirect_to @product, notice: "Review Successful"
+	  else
+	    redirect_to @product, notice: "Review Error"
+	  end
+
+	end
+
+  def review_params
+    params.require(:review).permit(
+    	:description,
+    	:rating,
+    )
   end
+
+    private
+ 
+    def require_login
+        unless @current_user
+            flash[:error] = "You must be logged in to access this section"
+            redirect_to '/signup' 
+        end
+    end
+
+
+
 end
